89c8a96672f65152de4d18c4b9decc582bf8b519
Marcus Ribeiro <marcus10dev@gmail.com>
2024-02-22 20:29:57 -0300

    Instalando dependências

---
+++ lib/src/pages/Books/Books.jsx
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = initializeApp({
+  apiKey: 'AIzaSyBqXsgDzYwAorpiOG2heBNirBisuNCvsr8',
+  authDomain: 'libfree-99914.firebaseapp.com',
+  projectId: 'libfree-99914',
+  storageBucket: 'libfree-99914.appspot.com',
+  messagingSenderId: '736654385798',
+  appId: '1:736654385798:web:518eb0de90a1dee22fa707',
+});
+
+const Books = () => {
+  return <div>Books</div>;
+};
+
+export default Books;
