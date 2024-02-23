1eeacaa2837b773647765782b3380279bfe76e14
Marcus Ribeiro <marcus10dev@gmail.com>
2024-02-23 19:07:59 -0300

    Estrutura page About

--- lib/src/pages/About/About.jsx
+++ lib/src/pages/About/About.jsx
@@ -1,8 +1,32 @@
 // CSS
-import 'About.css';
+import './About.scss';
 
-const About = () => {
-  return <div>About</div>;
-};
+function About() {
+  return (
+    <div className="text-box container d-flex flex-column justify-content-center">
+      <h2 className="text-title display-1 text-center text-white mb-5">Sobre</h2>
+      <p className="text-summary fs-4 text-center text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita fuga maxime ut cum
+        mollitia, repellat soluta distinctio! Ea cum delectus soluta quia aliquid ipsum repellat
+        animi nemo modi culpa, voluptates exercitationem quisquam inventore omnis assumenda error
+        doloribus doloremque accusamus dignissimos incidunt quasi reiciendis voluptatem sint
+        repudiandae! Consequuntur reprehenderit dolor quaerat debitis est temporibus, recusandae
+        doloremque ad ipsa atque ullam sapiente, nostrum dolorum. Voluptatibus aliquid nemo incidunt
+        temporibus quidem quo omnis tenetur, sapiente quisquam rem sunt odit illum explicabo, ipsam
+        ex hic obcaecati minus voluptates magnam molestias ea! Sit maxime similique accusamus ipsum
+        sequi aut quae, asperiores iusto quia corrupti magnam atque quos inventore voluptatem
+        reprehenderit minima ipsam iste facilis voluptatibus esse aliquid repellat. A totam iusto,
+        distinctio tempore aut magnam at delectus, eos tenetur impedit architecto deserunt dolorem
+        repellendus praesentium maxime doloribus corrupti blanditiis. Ad, a culpa unde odit quasi
+        voluptatem sunt nobis facere maxime possimus ex neque dolorem sint? Recusandae deleniti
+        quidem consequuntur porro voluptates magni nesciunt! Assumenda vel necessitatibus laborum
+        quibusdam beatae nulla quasi id ad accusamus officiis maxime tenetur praesentium facere
+        provident totam repellendus, velit, a cum atque quisquam incidunt debitis. Unde quisquam
+        vel, odio exercitationem odit eaque nobis! Reprehenderit quo deserunt fugit vero ipsum
+        laborum officiis!
+      </p>
+    </div>
+  );
+}
 
 export default About;
